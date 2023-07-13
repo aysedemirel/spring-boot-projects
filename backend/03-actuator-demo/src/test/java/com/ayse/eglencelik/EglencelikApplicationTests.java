8d4a2247103072458fcac26721b336698894f51e
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2023-07-13 22:09:34 +0300

    Add actuator demo

---
+++ backend/03-actuator-demo/src/test/java/com/ayse/eglencelik/EglencelikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayse.eglencelik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EglencelikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
