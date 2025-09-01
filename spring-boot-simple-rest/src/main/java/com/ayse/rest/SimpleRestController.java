518f5ab985f3a65fc35646f2dddf33cdb01f1da9
Ayşe Demirel Deniz <aysedemirel1996@gmail.com>
2025-09-01 22:33:22 +0500

    Add security and custom properties

--- spring-boot-simple-rest/src/main/java/com/ayse/rest/SimpleRestController.java
+++ spring-boot-simple-rest/src/main/java/com/ayse/rest/SimpleRestController.java
@@ -1,10 +1,15 @@
 package com.ayse.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleRestController {
+
+    @Value("${custom.world}")
+    private String customValue;
+
     @GetMapping("/")
     public String helloWorld(){
         return "Hello World!!!";
@@ -18,5 +23,10 @@ public class SimpleRestController {
     @GetMapping("/goodmorning")
     public String goodMorningWorld(){
         return "Good Morning World!!!";
+    }
+
+    @GetMapping("/custom")
+    public String customWorld(){
+        return "Hello " + customValue;
     }
 }
